17ff5fecca443f427eb1f418ef34d8aa1b45e75d
Randy <evilryok@gmail.com>
2021-12-10 17:36:52 +0800

    initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,51 @@
-//JQuery Module Pattern
 "use strict";
 
-// An object literal
 const app = {
-  init() {
-    app.functionOne();
-  },
-  functionOne() {
-  }
+    init() {
+        app.start();
+    },
+
+    start() {
+        var sidemenu = $('.side-menu');
+
+        var menu = {
+            open: function () {
+                setTimeout(function () {
+                    sidemenu.addClass('active');
+                    sidemenu.find('.wrap').addClass('active');
+                }, 200);
+            },
+            close: function () {
+                setTimeout(function () {
+                    sidemenu.removeClass('active');
+                    sidemenu.find('.wrap').removeClass('active');
+                }, 200);
+            }
+        }
+
+        $('.burger-icon').on('click', function(e) {
+            e.preventDefault();
+            menu.open();
+        });
+
+        $('.wrap-container').on('click', function(e) {
+            e.stopImmediatePropagation();
+        });
+
+        $(sidemenu).on('click', function () {
+            menu.close();
+        });
+
+        $(window).on('load resize orientationchange', function () {
+            var windowWidth = $(window).width();
+
+            if (windowWidth < 992) {
+                menu.close();
+            }
+        });
+    }
 };
 
-$("document").ready(() => {
-  app.init();
+$(function() {
+    app.init();
 });
